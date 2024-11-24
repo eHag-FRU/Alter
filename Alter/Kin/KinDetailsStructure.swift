90c24cde5acbcea3ea1b6fa2aff7f34b89a06913
Elliott Hager <ehager2@kent.edu>
2024-11-24 16:46:12 -0500

    Initial Commit

---
+++ Alter/Kin/KinDetailsStructure.swift
@@ -0,0 +1,17 @@
+//
+//  KinDetailsStructure.swift
+//  Alter
+//
+//  Created by Elliott Hager on 11/24/24.
+//
+
+import Foundation
+
+
+//
+//  This holds the structure of the Kin/Personality Profile to be written and
+//  read from a JSON object (the basis that the details will be saved in
+//
+struct KinDetailsStructure : Codeable, NSObject {
+    
+}
---
+++ Alter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alter
+//
+//  Created by Guest User on 11/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
